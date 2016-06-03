c2a871bfc4c47cc33b895c82ae87dce0e40659f4
Acer <Acer@Acer-V3-571G>
2016-06-03 11:00:44 +0300

    03-June-2016 - View layer implementation.

--- HauntedHotels/src/byui/cit260/hauntedHotels/view/MainMenuView.java
+++ HauntedHotels/src/byui/cit260/hauntedHotels/view/MainMenuView.java
@@ -5,14 +5,110 @@
  */
 package byui.cit260.hauntedHotels.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Acer
  */
 public class MainMenuView {
+    
+    private String menu;
+    private String promptMessage;
+   
+    public MainMenuView() {
+     this.menu = "\n"
+               + "\n--------------------------------------------"
+               + "\n| Main Menu                                |"
+               + "\n--------------------------------------------"
+               + "\nN - Start new game"
+               + "\nG - Get and start saved game"
+               + "\nH - Get help on how to play the game"
+               + "\nS - Save game"
+               + "\nQ - Quit"
+               + "\n--------------------------------------------";
+    }
+   
 
     void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; // exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; //initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is enter
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length()< 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; // end the loop
+        }
+        
+        return value; // return the value entered
+    }
+
+    private boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice) {
+            case "N": // create and start a new game
+                this.startNewGame();
+                break;
+            case "G": // get and start an existing game
+                this.startExistingGame();
+                break;
+            case "H": // display the help menu
+                this.displayHelpMenu();
+                break;
+            case "S": // save the current game
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+            // creat a new game
+        GameControl.createNewGame(HauntedHotels.getPlayer());
+        
+        //display the game menu
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** startExistingGame function called ***");
     }
     
 }
